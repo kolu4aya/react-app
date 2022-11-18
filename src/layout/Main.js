4e175c127a697b52312008080af29bff77a75f57
kolu4aya <43950592+kolu4aya@users.noreply.github.com>
2022-11-18 15:34:37 +0600

    chang 2

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -2,36 +2,36 @@ import React from 'react'
 import Movies from '../components/Movies'
 import Search from '../components/Search/search'
 
-// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 // console.log(process.env)
 class Main extends React.Component {
     state = {
         movies: [],
     }
-// componentDidMount() {
-//     console.log(process.env)
-//     fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
-//     .then(resp => resp.json())
-//     .then(data => this.setState({movies:data.Search}))
-// }
-
-// searchMovies = (str, type = 'all') => {
-//     fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !=='all' ? `&type=${type}`: ''}`)
-//     .then(resp => resp.json())
-//     .then(data => this.setState({movies:data.Search}))
-// }
 componentDidMount() {
     console.log(process.env)
-    fetch(`http://www.omdbapi.com/?apikey=4082564b&s=matrix`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
     .then(resp => resp.json())
     .then(data => this.setState({movies:data.Search}))
 }
 
 searchMovies = (str, type = 'all') => {
-    fetch(`http://www.omdbapi.com/?apikey=4082564b&s=${str}${type !=='all' ? `&type=${type}`: ''}`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !=='all' ? `&type=${type}`: ''}`)
     .then(resp => resp.json())
     .then(data => this.setState({movies:data.Search}))
 }
+// componentDidMount() {
+//     console.log(process.env)
+//     fetch(`http://www.omdbapi.com/?apikey=4082564b&s=matrix`)
+//     .then(resp => resp.json())
+//     .then(data => this.setState({movies:data.Search}))
+// }
+
+// searchMovies = (str, type = 'all') => {
+//     fetch(`http://www.omdbapi.com/?apikey=4082564b&s=${str}${type !=='all' ? `&type=${type}`: ''}`)
+//     .then(resp => resp.json())
+//     .then(data => this.setState({movies:data.Search}))
+// }
     render(){
         const {movies} = this.state;
           return <main className = 'main'>
